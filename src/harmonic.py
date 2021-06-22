2df2f0545173d1cae42e5c010fe00b584edde9d4
antonio_cruciani <antonio.cruciani@gssi.it>
2021-06-22 10:50:04 +0200

    Definition of some classes for the harmonic.py

--- src/harmonic.py
+++ src/harmonic.py
@@ -1,5 +1,49 @@
 import networkit as nk
 
+
+
+class FairHarmonicCentrality:
+
+    def __init__(self, G, C, k, TopHarmonicCloseness=None):
+        if TopHarmonicCloseness is None:
+            self.TopHarmonicCloseness = {"top": True, "useNBbound": False,'normalized':False}
+        else:
+            self.TopHarmonicCloseness = TopHarmonicCloseness
+        self.G = G
+        self.C = C
+        self.k = k
+        self.HC = None
+        # Group Harmonic Centrality and Fair Group Harmonic Centrality
+        self.S = None
+        self.GHC = None
+        self.FGHC = None
+
+    def compute_centrality(self):
+        #Compute the Centrality measure
+        if(self.TopHarmonicCloseness['top']):
+
+            self.HC  = nk.centrality.TopHarmonicCloseness(self.G,self.k,useNBbound= self.TopHarmonicCloseness['useNBbound'])
+        else:
+            #Computing centrality exhaustively
+            self.HC = nk.centrality.HarmonicCloseness(self.G,normalized= self.TopHarmonicCloseness['normalized'],)
+        self.HC.run()
+
+    def compute_top_k_fair_group_centrality(self):
+        # Computing the fairness level of the first k
+        self.S = self.HC.topkNodesList()
+        # For each community, get the group centrality of each community wrt the set S
+        fairness = []
+        for community in self.C:
+            group = list(set(community).intersection(set(self.S)))
+            if(len(group)>0):
+                SG = self.G.subgraphFromNodes(group)
+            else:
+                fairness.append(0)
+
+        print("Miao")
+
+
+
 G = nk.graphio.SNAPGraphReader().read("../datasets/com-youtube/com-youtube.ungraph.txt")
 # Kadabra is apx betweennes
 kadabra = nk.centrality.KadabraBetweenness(G,0.05,0.8)
